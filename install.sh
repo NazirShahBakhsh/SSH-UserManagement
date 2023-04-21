7514b45cd3563a88155db660892c1164cfe47aaf
InternetCables <130496060+InternetCables@users.noreply.github.com>
2023-04-21 19:59:38 +0430

    Update

--- install.sh
+++ install.sh
@@ -185,7 +185,7 @@ sudo phpenmod curl
 PHP_INI=$(php -i | grep /.+/php.ini -oE)
 sed -i 's/extension=intl/;extension=intl/' ${PHP_INI}
 fi
-bash <(curl -Ls https://raw.githubusercontent.com/HamedAp/ioncube-loader/main/install.sh --ipv4)
+bash <(curl -Ls https://raw.githubusercontent.com/InternetCables/SSH-UserManagement/main/IoncubeLoader/install.sh --ipv4)
 bash <(curl -Ls https://raw.githubusercontent.com/HamedAp/Nethogs-Json/main/install.sh --ipv4)
 mysql -e "create database ShaHaN;" &
 wait
